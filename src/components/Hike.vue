9f6ff7e5c2f71595a94dac34ce7e4448c9e48c3b
rebeccapeltz <rebeccapeltz@gmail.com>
2018-05-23 12:54:26 -0700

    add google map

--- src/components/Hike.vue
+++ src/components/Hike.vue
@@ -1,7 +1,8 @@
 /* eslint-disable */
 <template>
-  <div class="hike container">
+  <div class="hike container text-info">
         <h2>{{ hike.name }}</h2>
+      <div class="col-xs-12 col-md-6"> 
     <b>Link: </b>
     <div><a :href="hike.link" target="_blank">{{ hike.link }}</a></div>
     <b>Elevation: </b>
@@ -12,32 +13,63 @@
     <div>{{ hike.longitude }}</div>
     <b>Length: </b>
     <div>{{ hike.length }}</div>
+    </div>
+    <div class="col-xs-12 col-md-6">
+      <div id="map"></div>
+      </div>
      <br/>
+     <div class="col-xl-12">
     <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
     <router-link v-bind:to="'/'">Back to hike list</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapList } from '../data';
+import { mapList } from "../data";
+const loadGoogleMapsApi = require('load-google-maps-api')
 export default {
-  name: 'hike',
-  data () {
-    return {hike: mapList.getItemByKey(this.$route.params.hike_id),hikes: mapList};
+  name: "hike",
+  data() {
+    return {
+      hike: mapList.getItemByKey(this.$route.params.hike_id),
+      hikes: mapList
+    };
+  },
+  mounted() {
+    let self = this;
+    loadGoogleMapsApi()
+      .then(function(googleMaps) {
+        new googleMaps.Map(document.querySelector("#map"), {
+          key: 'AIzaSyCnBaP_WQB9R6-4ilksoMB1VuTEAgJRtco',
+          center: {
+            lat: self.hike.latitude,
+            lng: self.hike.longitude
+          },
+          zoom: 12
+        });
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
   }
-}
+};
+// AIzaSyCnBaP_WQB9R6-4ilksoMB1VuTEAgJRtco
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .form-group {
-      max-width: 500px;
-    }
-
-    .actions {
-      padding: 10px 0;
-    }
-
-  
-
+  max-width: 500px;
+}
+.text-info {
+  text-align: left;
+}
+.actions {
+  padding: 10px 0;
+}
+#map {
+  height: 400px;
+  width: 100%;
+}
 </style>
